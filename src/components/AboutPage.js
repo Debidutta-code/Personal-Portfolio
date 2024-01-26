7185d99ecd0cd445716198274ba77d551c74bc66
DEBIDUTTA ACHARYA <77004029+Debidutta-code@users.noreply.github.com>
2024-01-26 23:59:54 +0530

    third push

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -5,11 +5,20 @@ import '../styles/AboutPage.css';
 const AboutPage = () => {
     return (
         <div id="about" className="about-page">
-            <h1>About Me</h1>
-            <p>
-                Welcome to my website! I am a passionate Full Stack Web Developer
-                and a problem solver. Learn more about my skills and experiences here.
-            </p>
+            <h1 className="name-heading">About Me</h1>
+            <div className="about-section">
+                <div className="image-container">
+                    <div className="image">
+
+                    </div>
+                </div>
+                <div className="about-container">
+                    
+                    <div className="about">
+                        m Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
